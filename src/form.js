63c66588bd1bb09c6bceaffdfaf98c257457da56
arunmadhavan-g <arunmadhavan.g@gmail.com>
2019-12-09 14:10:46 +0530

    [Arun] | Changes the code to display the list of installed software in the dropdown

--- src/form.js
+++ src/form.js
@@ -1,59 +1,75 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './form.css';
 import Form from "react-jsonschema-form";
+import axios from "axios";
 
-const schema = {
-  "title": "Software License form",
-  "description": "License form for software",
-  "type": "object",
-  "src" : "http://localhost:3000",
-  "required": [
-    "softwareName",
-    "softwareLicense"
-  ],
-  "properties": {
+const schema = softwares => ({
+    "title": "Software License form",
+    "description": "License form for software",
+    "type": "object",
+    "src": "http://localhost:3000",
+    "required": [
+        "softwareName",
+        "softwareLicense"
+    ],
+    "properties": {
+        "softwareName": {
+            "title": "Software name",
+            "type": "string",
+            "enum": softwares.map(x => x.software_name),
+            "enumName": softwares.map(x => x.id),
+            "default": ""
+        },
+        "softwareLicense": {
+            "title": "Software license",
+            "type": "string",
+            "enum": ['licensed', 'opensource'],
+            "enumName": ['licensed', 'opensource'],
+            "default": "licensed"
+        },
+        "numberOfLicenses": {
+            "type": "integer",
+            "title": "Number of licenses"
+        }
+    }
+});
+const uiSchema = {
     "softwareName": {
-      "title": "Software name",
-      "type": "string",
-      "enum": [{
-        "$ref": "#/software_name"
-      }],
-      "default": ""
+        "ui:widget": "select"
     },
     "softwareLicense": {
-      "title": "Software license",
-      "type": "string",
-      "enum": ['licensed', 'opensource'],
-      "enumName": ['licensed', 'opensource'],
-      "default": "licensed"
+        "ui:widget": "select"
     },
     "numberOfLicenses": {
-      "type": "integer",
-      "title": "Number of licenses"
+        "ui:widget": "updown"
     }
-  }
-};
-const uiSchema = {
-  "softwareName": {
-    "ui:widget": "select"
-  },
-  "softwareLicense": {
-    "ui:widget": "select"
-  },
-  "numberOfLicenses": {
-    "ui:widget": "updown"
-  }
 };
 
-const App = (props) => (
-  <div>
-    <Form schema={schema}
-      uiSchema={uiSchema}
-      onChange={(e) => console.log("changed", e)}
-      onSubmit={(e) => console.log("submitted", e)}
-      onError={() => console.log("errors")}
-    />
-  </div>
-);
+const App = () => {
+    const [softwares, setSoftwares] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/installed_software`)
+            .then(res => {
+                setSoftwares(res.data);
+            })
+    }, []);
+
+    return (
+        <div>
+            <Form schema={schema(softwares)}
+                  uiSchema={uiSchema}
+                  onChange={(e) => console.log("changed", e)}
+                  onSubmit={(e) => {
+                      const data = e.formData;
+                      axios.post(`http://localhost:3000/software_license`, {
+                          // TODO: Map the response with the table post
+                      })
+                  }}
+                  onError={() => console.log("errors")}
+            />
+        </div>
+    );
+};
 export default App;
 
--- src/test.js
+++ src/test.js
@@ -1,24 +1,5 @@
 import React from 'react';
-import axios from 'axios';
 
-export default class softwareList extends React.Component {
-    state = {
-        software: []
-    }
+const softwareList = () => <div>Hey</div>;
 
-    componentDidMount() {
-        axios.get(`http://localhost:3000/installed_software`)
-            .then(res => {
-                const software = res.data;
-                this.setState({ software });
-            })
-    }
-
-    render() {
-        return (
-            <select>
-                {this.state.software.map(software => <option>{software.software_name}</option>)}
-            </select>
-        )
-    }
-}
+export default softwareList;
